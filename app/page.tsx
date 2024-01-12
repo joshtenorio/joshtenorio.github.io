1c0054c05257a1f65c54b69d39a5232d9364b5f3
Joshua Tenorio <joshliam09@gmail.com>
2024-01-11 23:30:58 -0700

    fix more things

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
   return (
     <div className="mx-auto max-w-xl py-8">
       <h1 className="mb-4 text-center text-2xl font-black">
-        josh's tech journey
+        {`josh's tech journey`}
       </h1>
       <p className='mb-10 text-center'>{`Hi, I'm Josh. Thanks for visiting, and please mind the dust while I finish this website :)`}</p>
       {posts.map((post, idx) => (
