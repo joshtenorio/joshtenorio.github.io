9bb944dfa3df849bbf4346bdcab8133bef3fb786
Joshua Tenorio <joshliam09@gmail.com>
2024-01-11 22:15:21 -0700

    hehehe

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Button } from './ui/button'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>
+        <Button>jxorio</Button>
+        <Button>Posts</Button>
+        <Button>Projects</Button>
+        <Button>About</Button>
+    </div>
   )
 }
 
