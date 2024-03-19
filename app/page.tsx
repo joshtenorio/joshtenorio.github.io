172f3226f6a91fbbffd353b223c16de598b77ecc
Joshua Tenorio <joshliam09@gmail.com>
2024-03-19 16:10:10 -0700

    memes!

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
-import Link from 'next/link'
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
-import { Badge } from '@/components/ui/badge'
-
 
 export default function Home() {
-
   return (
     <div className="mx-auto max-w-xl py-8">
       <h1 className="mb-4 text-center text-2xl font-black">
         {`josh's tech journey`}
       </h1>
-      <p className='mb-10 text-center'>{`Hi, I'm Josh. Thanks for visiting, and please mind the dust while I finish this website :)`}</p>
+      <h2 className="text-xl text-center">Hi, I'm Josh -</h2>
+      <p>
+        - Software Engineer
+      </p>
+      <p>
+        - PCB Designer
+      </p>
+      <p>
+        - triple digit iRating sim racer
+      </p>
     </div>
   )
 }
