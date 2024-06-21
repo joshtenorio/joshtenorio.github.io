4b505b916ea15e7d67901a46360cb8cc170efc77
Joshua Tenorio <joshliam09@gmail.com>
2024-06-20 19:16:36 -0700

    testing dev env on linux

---
+++ app/ui/page.tsx
@@ -0,0 +1,10 @@
+function Page() {
+  return (
+    <div>
+      <div className="text-2xl text-center py-4">UI Snippets</div>
+      <div className="text-center py-4">File Table</div>
+    </div>
+  );
+}
+
+export default Page;
